05f90c22b3c38aad60dc1a2a680c82ef857ca36e
Lance Halvorsen <lance.halvorsen@gmail.com>
2014-08-21 14:24:16 -0700

    removes redundant, unused routes from sub-routers

--- apps/feed/web/router.ex
+++ apps/feed/web/router.ex
@@ -1,6 +1,3 @@
 defmodule Feed.Router do
   use Phoenix.Router
-
-  get "/", Feed.PageController, :index, as: :pages
-
 end
--- apps/post/web/router.ex
+++ apps/post/web/router.ex
@@ -1,6 +1,3 @@
 defmodule Post.Router do
   use Phoenix.Router
-
-  get "/", Post.PageController, :index, as: :pages
-
 end
